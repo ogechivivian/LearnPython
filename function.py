29535199e3d97e9bafb8dd8146a63870c1fcae03
Vivian <okeyogechivivian@gmail.com>
2025-04-04 10:13:36 +0200

    Created a function to return sum of two numbers

---
+++ function.py
@@ -0,0 +1,7 @@
+def addnum():
+    first_number = int(input("Enter first number: "))
+    second_number = int(input("Enter second number: "))
+    sum = first_number + second_number
+    print(sum)
+
+addnum()
