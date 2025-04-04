2e21c15fa340538429eca9f4a76b0920ba35f64c
Vivian <okeyogechivivian@gmail.com>
2025-04-04 10:15:13 +0200

    Created a function to return multiplication of two numbers

---
+++ multiplication.py
@@ -0,0 +1,5 @@
+def multiplication(num1, num2):
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    multiplication = num1 * num2
+    print(multiplication)
